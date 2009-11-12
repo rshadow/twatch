87b9ef3aca91418b57c2c83f5f0f6354799b8132
nikolaevroman <nikolaevroman@7c213f76-d853-476d-b686-6375e4b93349>
2009-11-12 15:49:58 +0000

    Settings dialog complete
    git-svn-id: file:///mnt/system/svn/twatch@15 7c213f76-d853-476d-b686-6375e4b93349

--- trunk/twatch-gtk/lib/TWatchGtk/Controller/Settings.pm
+++ trunk/twatch-gtk/lib/TWatchGtk/Controller/Settings.pm
@@ -20,10 +20,7 @@ sub init
 
     # Строковые параметры
     $self->{builder}->get_object( $_ )->set_text( config->daemon->get_orig( $_ ) )
-        for (qw(Project Save Complete));
-
-    $self->{builder}->get_object( $_ )->set_text( config->daemon->get( $_ ) )
-        for (qw(EMail));
+        for (qw(Project Save Complete EMail));
 
     # Уровень отсылки уведомлений
     for( @{ config->daemon->get('EmailLevel') } )
@@ -42,15 +39,81 @@ sub on_button_ok_pressed
 {
     my ($self, $window) = @_;
 
-    # Сохраним
+    # Массив с новыми пользовательскими параметрами
+    my @config;
+
+    # Если что-то изменилось то сохраним в локальном файле настроек пользователя
+
+    # Строковые параметры
+    for (qw(Project Save Complete EMail))
+    {
+        my $value = $self->{builder}->get_object( $_ )->get_text();
+
+        push @config, {name => $_, value => $value}
+            if config->daemon->get_orig( $_ ) ne $value;
+    }
+
+    # Уровень отсылки уведомлений
+    {
+        my @value;
+        for my $type ( qw(info error) )
+        {
+            my $new = $self->{builder}->get_object($type)->get_active();
+            my $old = grep {$_ eq $type} @{ config->daemon->get('EmailLevel') };
+            push @value, $type if $new != $old;
+        }
+        push @config, {name => 'EmailLevel', value => join ',', @value}
+            if @value;
+    }
+
+    # Использование прокси
+    {
+        my $value = $self->{builder}->get_object('NoProxy')->get_active();
+        push @config, {name => 'NoProxy', value => $value}
+            if config->daemon->is_noproxy != $value;
+    }
+
+    # Выйдем если пользователь ничего не менял
+    $self->{window}->destroy,
+    return TRUE
+        unless @config;
+
+    # Найдем подходящий (в директории пользователя) путь к файлу конфигурации
+    my $config;
+    for ( @{ config->daemon->{dir}{config} } )
+    {
+        next unless m/^$ENV{HOME}/;
+        # Удалим home
+        s/^~/$ENV{HOME}/;
+        $config = $_;
+        last;
+    }
+
+    warn sprintf('Can`t find path to store user config file'),
+    $self->{window}->destroy,
+    return FALSE
+        unless $config;
+
+    # Запишем конфиг в файл
+    open my $file, '>', $config
+        or warn sprintf('Can`t save config file %s : %s', $config, $!);
+
+    $self->{window}->destroy,
+    return FALSE
+        unless $file;
+
+    print $file sprintf( "%s = %s\n", $_->{name}, $_->{value} ) for @config;
+    close $file;
 
     $self->{window}->destroy;
+    return TRUE;
 }
 
 sub on_button_cancel_pressed
 {
     my ($self, $window) = @_;
     $self->{window}->destroy;
+    return TRUE;
 }
 
 1;
